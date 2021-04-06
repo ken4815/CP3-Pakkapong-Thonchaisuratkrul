8ea08f02c1455ad8a924fc227584b5352e0072e0
ken4815 <79741230+ken4815@users.noreply.github.com>
2021-04-06 22:09:11 +0700

    Add files via upload

---
+++ Excercise4_Pakkapong_T.py
@@ -0,0 +1,12 @@
+'''
+S=Subject
+'''
+S1= "Foundation English"
+S2= "General Business"
+S3= "Introduction to Computer Systems"
+S4= "Computer Programming"
+print("---------Your Score--------")
+print(S1+":",81.75)
+print(S2+":",79.99)
+print(S3+":",82.25)
+print(S4+":",99.99)
